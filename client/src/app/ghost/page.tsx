efbe3890f8600655334a660377722627e563d319
VishalMGodi <97684737+VishalMGodi@users.noreply.github.com>
2024-03-04 18:38:38 +0530

    client setup

---
+++ client/src/app/ghost/page.tsx
@@ -0,0 +1,21 @@
+'use client';
+import axios from 'axios';
+import React from 'react'
+
+function Ghost() {
+    const handleClick = ()=>{
+        console.log('SENDING');
+        axios.get('http://localhost:8080/api/hello', ).then((response)=> {
+          console.log(response);
+        }).catch((error)=> {
+          console.log(error);
+        });
+    }   
+  return (
+    <div className='bg-green-500 p-3 m-2 flex flex-row gap-5 justify-center font-bold'>
+        <button onClick={handleClick} className='bg-red-500 p-2 rounded-sm'>Click me</button>
+    </div>
+  )
+}
+
+export default Ghost
