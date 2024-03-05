cbbd9c4c38fd1397777273335d732f105bdeeca5
Varun Kamath <mail.varunkamath@gmail.com>
2024-03-05 13:32:57 +0530

    Sidebar Update

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -9,18 +9,23 @@ export default function Sidebar() {
     <>
       <aside
         id="sidebar"
-        className="fixed lg:relative top-0 left-0 z-40 w-[15rem] lg:w-[17.5rem] h-screen transition-transform translate-x-0 border-r-4 lg:translate-x-0 border-gray-700 flex flex-col gap-3"
+        className="fixed lg:relative bg-blue-950 top-0 left-0 z-40 w-[15rem] lg:w-[17.5rem] h-screen transition-transform translate-x-0 border-r-4 lg:translate-x-0 border-gray-700 flex flex-col gap-10"
         style={{ paddingTop: "70px" }}
       >
-        {allowedRoutes.map((links, index) => (
-          <StyledLink
-            href={links.link}
-            key={index}
-            className="flex flex-row gap-5 border-t border-b border-gray-500 p-5"
-          >
-            {links.name}
-          </StyledLink>
-        ))}
+        <div className="flex justify-center">
+          <p className="text-white font-semibold text-xl">Project Management</p>
+        </div>
+        <div className="flex flex-col gap-3">
+          {allowedRoutes.map((links, index) => (
+            <StyledLink
+              href={links.link}
+              key={index}
+              className="flex flex-row gap-5 border-white p-5 text-white hover:bg-blue-900 transition-all duration-200"
+            >
+              {links.name}
+            </StyledLink>
+          ))}
+        </div>
       </aside>
     </>
   );
