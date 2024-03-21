f17a6dcfd575f7db738326097308d334cb97982b
RockerBot <rockerbotmail@gmail.com>
2024-03-21 17:51:53 +0530

    Added AddButton

---
+++ client/src/components/atoms/AddButton.tsx
@@ -0,0 +1,18 @@
+import { Children, MouseEventHandler } from "react";
+import { MdAddChart } from "react-icons/md";
+interface AddButtonProps
+    extends Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, "href"> {
+    onclick: MouseEventHandler;
+}
+export default function AddButton(props: AddButtonProps){
+    return (
+        <button 
+            className="flex border-2 items-center gap-2 px-4 py-1 rounded-sm hover:bg-gray-700 hover:text-white transition-all duration-200"
+            onClick={props.onclick}
+        >
+        <span>
+            <MdAddChart />
+        </span>
+        {props.children}
+        </button>);
+}
