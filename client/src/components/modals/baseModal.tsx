649108bccdab4303211bbe49f280804cfa49e879
RockerBot <rockerbotmail@gmail.com>
2024-03-28 13:07:32 +0530

    Centered BaseModal

--- client/src/components/modals/baseModal.tsx
+++ client/src/components/modals/baseModal.tsx
@@ -9,23 +9,25 @@ interface baseModalProps
 
 const BaseModal = (props: baseModalProps) => {
   return (
-    <div className="transition-all duration-200 lg:-translate-x-1/2 fixed transform bg-white rounded-md shadow-2xl lg:w-1/3 md:w-2/3 w-full overflow-hidden z-50">
-      <div className="px-4 py-3 bg-slate-800 rounded-t-md flex justify-between items-center">
-        <div>
-          <h2 className="text-lg font-semibold text-white">
-            {props.modalTitle}
-          </h2>
+    <div className=" fixed w-screen h-screen flex justify-center items-center bg-opacity-50 bg-black left-0 top-0 ">
+      <div className="transition-all duration-200 transform bg-white rounded-md shadow-2xl lg:w-1/3 md:w-2/3 w-full overflow-hidden z-50 opacity-100">
+        <div className="px-4 py-3 bg-slate-800 rounded-t-md flex justify-between items-center">
+          <div>
+            <h2 className="text-lg font-semibold text-white">
+              {props.modalTitle}
+            </h2>
+          </div>
+          <button
+            onClick={() => props.setShowModal(false)}
+            className="cursor-pointer border-2 hover:text-red-500 hover:bg-gray-200 transition-all duration-200 p-1 flex items-center rounded-lg text-xs text-white"
+          >
+            <IoCloseSharp className="h-5 w-5 " />
+          </button>
         </div>
-        <button
-          onClick={() => props.setShowModal(false)}
-          className="cursor-pointer border-2 hover:text-red-500 hover:bg-gray-200 transition-all duration-200 p-1 flex items-center rounded-lg text-xs text-white"
-        >
-          <IoCloseSharp className="h-5 w-5 " />
-        </button>
-      </div>
-      <div className="overflow-y-auto max-h-[33rem]">
-        <div className="p-10">
-            {props.children}
+        <div className="overflow-y-auto max-h-[33rem]">
+          <div className="p-10">
+              {props.children}
+          </div>
         </div>
       </div>
     </div>
