778b63405379ad54e38142afc9bcc2706fbb6dfe
RockerBot <rockerbotmail@gmail.com>
2024-04-02 10:23:09 +0530

    trying out pretier

--- client/src/app/(application)/home/page.tsx
+++ client/src/app/(application)/home/page.tsx
@@ -1,7 +1,7 @@
-"use client";
-import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
-import React from "react";
+'use client';
+import { useSession } from 'next-auth/react';
+import { redirect } from 'next/navigation';
+import React from 'react';
 
 export default function Home() {
   const { data: session } = useSession({
@@ -12,7 +12,7 @@ export default function Home() {
   });
   const user = session?.user;
   return (
-    <section className="flex flex-row gap-5 mt-4">
+    <section className='flex flex-row gap-5 mt-4'>
       <div>Hello {user ? JSON.stringify(user) : null}</div>
     </section>
   );
